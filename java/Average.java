ec8bf8ac4ae68ed8224edc1cd801ac426436421f
Namita <aroranamita33@gmail.com>
2019-03-04 17:38:34 +0530

    ch

---
+++ java/Average.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+import java.util.Scanner;
+
+public class Average{
+public String Average(int num1)
+{
+	
+}
+}
+
+
+
+
---
+++ test/AverageTest.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AverageTest {
+    @Test
+    public void testReverseString() throws Exception{
+        String expectedValue = "The average is:81.50";
+        Average data=new Average();
+        String actualValue=data.findAverage("4");
+        assertEquals(expectedValue, actualValue);
+
+}}
